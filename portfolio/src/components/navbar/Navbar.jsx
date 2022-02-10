c74623310c2e41142e37bf29a03df5ff6d9072e0
B-Werth <bastian619werth@gmail.com>
2022-02-10 18:59:26 +0100

    update

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
+import logo from "../../assets/logo.jpg";
 
-function navbar() {
-  return <div>navbar</div>;
-}
+const navbar = () => {
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+            <a href="#home">Home</a>
+            <a href="#home">Home</a>
+            <a href="#home">Home</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default navbar;
