8a2948510d454611f901938baff41f84eba5cdf8
B-Werth <bastian619werth@gmail.com>
2022-02-12 23:36:06 +0100

    chakra ui

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -1,24 +1,59 @@
 import React from "react";
-import "./navbar.css";
-import logo from "../../assets/logo.jpg";
+
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue,
+  Center,
+  Text,
+  HStack,
+} from "@chakra-ui/react";
 
 const navbar = () => {
   return (
-    <div className="gpt3__navbar">
-      <div className="gpt3__navbar-links">
-        <div className="gpt3__navbar-links_logo">
-          <img src={logo} alt="logo" />
-        </div>
-
-        <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#home">Home </a>
-            <a href="#home">Home </a>
-            <a href="#home">Home </a>
-            <a href="#home">Home </a>
-          </p>
-        </div>
-      </div>
+    <div>
+      <Box
+        w="100%"
+        h={"5%"}
+        p={4}
+        color="white"
+        position="fixed"
+        zIndex={5}
+        css={{ backdropFilter: "blur(10px)" }}
+        bg={"#040c1840"}
+      >
+        <Container
+          display="flex"
+          p={2}
+          maxW="container.md"
+          wrap="wrap"
+          align="center"
+          justify="space-between"
+        >
+          <Center>
+            <Flex align="center" mr={5}>
+              <Heading as="h1" size="lg" letterSpacing={"tighter"}>
+                B-Werth
+              </Heading>
+            </Flex>
+            <HStack spacing="100px">
+              <Text>Projekte</Text>
+              <Text>Posts</Text>
+              <Text>Github</Text>
+              <Text>jo</Text>
+            </HStack>
+          </Center>
+        </Container>
+      </Box>
     </div>
   );
 };
