a273825b1d48718429f566cf6c0e3211e55f370f
B-Werth <bastian619werth@gmail.com>
2022-02-13 22:12:03 +0100

    m

--- portfolio/src/components/navbar/Navbar.jsx
+++ portfolio/src/components/navbar/Navbar.jsx
@@ -49,7 +49,7 @@ const navbar = () => {
               <Text>Projekte</Text>
               <Text>Posts</Text>
               <Text>Github</Text>
-              <Text>jo</Text>
+              <Text>joj</Text>
             </HStack>
           </Center>
         </Container>
